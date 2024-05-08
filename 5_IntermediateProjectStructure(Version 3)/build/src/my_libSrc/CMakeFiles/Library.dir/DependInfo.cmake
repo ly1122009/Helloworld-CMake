8c6e3302e924b831443f479e06748cfc513105a8
lynguyen <ly1122009@gmail.com>
2024-05-08 17:31:21 +0700

    Update Intermediate Project Structure Version 3

---
+++ 5_IntermediateProjectStructure(Version 3)/build/src/my_libSrc/CMakeFiles/Library.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lynguyen/Cmake/github/Helloworld-CMake/5_IntermediateProjectStructure(Version 3)/src/my_libSrc/func.cc" "src/my_libSrc/CMakeFiles/Library.dir/func.cc.o" "gcc" "src/my_libSrc/CMakeFiles/Library.dir/func.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
