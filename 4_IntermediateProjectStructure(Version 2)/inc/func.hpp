8ec9ace0cc1d8ac3ec0d9d51855fa9f601793eb5
lynguyen <ly1122009@gmail.com>
2024-05-08 17:19:45 +0700

    Update Intermediate Project Structure Version 2

---
+++ 4_IntermediateProjectStructure(Version 2)/inc/func.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+auto print_helloworld(void) -> void;
---
+++ 4_IntermediateProjectStructure(Version 2)/src/func.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "func.hpp"
+
+auto print_helloworld(void) -> void
+{
+    std::cout << "Hello World!\n";
+}
