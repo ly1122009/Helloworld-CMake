34afbf0ca8b9e0be06679edfef238445671b4e1e
lynguyen <ly1122009@gmail.com>
2024-05-11 20:58:20 +0700

    Update API documentation doxygen Part 1

---
+++ 8_Doxygen/inc/my_lib.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstdint>
+/**
+ * @brief Sum Calculator  
+ * @param[in] p_num1: The first element 
+ * @param[in] p_num2: The second element
+ * 
+ * @return uint32_t : Value of sum p_num1 and p_num2
+ */
+auto cal_sum(uint32_t p_num1, uint32_t p_num2) -> uint32_t;
+
