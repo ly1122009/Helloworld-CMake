11c1ee02d79ea832a21359558f87eb9d5518bd91
lynguyen <ly1122009@gmail.com>
2024-05-14 16:27:21 +0700

    Update catch2 unit test offline

---
+++ 10_Catch2_Offline/docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cal_5fsum_0',['cal_sum',['../my__lib_8cc.html#a16f1c3278ed99d4f3198ea3472ac7c1a',1,'cal_sum(uint32_t p_num1, uint32_t p_num2) -&gt; uint32_t:&#160;my_lib.cc'],['../my__lib_8hpp.html#a16f1c3278ed99d4f3198ea3472ac7c1a',1,'cal_sum(uint32_t p_num1, uint32_t p_num2) -&gt; uint32_t:&#160;my_lib.cc']]],
+  ['cmakelists_2etxt_1',['CMakeLists.txt',['../src_2CMakeLists_8txt.html',1,'(Global Namespace)'],['../inc_2CMakeLists_8txt.html',1,'(Global Namespace)']]]
+];
---
+++ 10_Catch2_Offline/src/my_lib.cc
@@ -0,0 +1,12 @@
+#include "my_lib.hpp"
+#include <iostream>
+
+auto cal_sum(uint32_t p_num1, uint32_t p_num2) -> uint32_t
+{
+    return p_num1 + p_num2;
+}
+
+auto cal_sub(uint32_t p_num1, uint32_t p_num2) -> uint32_t
+{
+    return p_num1 - p_num2;
+}
