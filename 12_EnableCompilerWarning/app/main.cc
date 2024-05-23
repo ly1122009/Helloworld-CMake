d05bc0f51b6056ebb647d5f6410d40ea40930e03
lynguyen <ly1122009@gmail.com>
2024-05-23 14:55:48 +0700

    Update Enable Warning

---
+++ 12_EnableCompilerWarning/app/main.cc
@@ -0,0 +1,10 @@
+#include "main.hpp"
+
+int main(void)
+{
+    int i;
+    std::cout << "Sum of 5 + 4 = " << cal_sum(5,4) << std::endl;
+    std::cout << "Sub of 5 - 4 = " << cal_sub(5,4) << std::endl;
+    return TRUE;
+}
+
